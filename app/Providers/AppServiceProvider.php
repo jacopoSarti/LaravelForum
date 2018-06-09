f5470ab2067ec433aded069a8063cb8b4a21dec9
Jacopo Sarti <jacopo.sarti90@gmail.com>
2018-06-09 20:28:52 +0800

    End of Lesson 20 - From 56 Queries Down to 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Channel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::share('channels', \App\Channel::all());
+        \View::composer('*', function($view){
+           $channels = \Cache::rememberForever('channels', function(){
+               return Channel::all();
+           });
+
+           $view->with('channels', $channels);
+        });
     }
 
     /**
@@ -23,6 +30,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->isLocal())
+        {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -29,7 +29,9 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class);
+        return $this->hasMany(Reply::class)
+            ->WithCount('favourites')
+            ->with('owner');
     }
 
     public function addReply($reply)
